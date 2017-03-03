899b31e5656c474439e1e876ea1dfa05ad66e5ca
Paul Tagliamomnte <tag@pault.ag>
2017-03-03 16:17:29 -0500

    import

--- server.go
+++ server.go
@@ -1,19 +1,61 @@
 package main
 
 import (
-	"io"
+	"encoding/pem"
+	"html/template"
+	"log"
 	"net/http"
 
 	"crypto/tls"
+	"crypto/x509"
 )
 
+func handleError(w http.ResponseWriter, req *http.Request, err error) error {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(500)
+	w.Write([]byte(err.Error()))
+	return nil
+
+}
+
+type Certificates struct {
+	Certificates []*x509.Certificate
+}
+
 func LintServer(w http.ResponseWriter, req *http.Request) {
-	for _, cert := range req.TLS.PeerCertificates {
-		io.WriteString(w, cert.Subject.CommonName)
+	pages, err := template.New("").Funcs(template.FuncMap{
+		"toPEM": func(type_ string, data interface{}) (string, error) {
+			der, err := x509.MarshalPKIXPublicKey(data)
+			if err != nil {
+				return "", err
+			}
+			pemBytes := pem.EncodeToMemory(&pem.Block{
+				Type:  type_,
+				Bytes: der,
+			})
+			if err != nil {
+				return "", err
+			}
+			return string(pemBytes), nil
+		},
+	}).ParseFiles("templates/index.html")
+	if err != nil {
+		handleError(w, req, err)
+		return
 	}
+
+	if err = pages.ExecuteTemplate(w, "index.html", Certificates{
+		Certificates: req.TLS.PeerCertificates,
+	}); err != nil {
+		log.Printf("%s\n", err)
+		return
+	}
+	return
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", LintServer)
 
 	tlsConfig := &tls.Config{ClientAuth: tls.RequireAnyClientCert}
